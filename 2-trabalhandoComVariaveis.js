f8bb37aa7d093bebb4935f728b8d95b35f359296
Willians Santos <62728045+willianspesantos@users.noreply.github.com>
2020-07-22 12:17:28 -0300

    Trabalhando com Variáveis
    Tipos de variáveis.

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,10 @@
+//Console.log("meu primeiro programa: Trabalhando com variaveis");
+console.log("meu primeiro programa: Trabalhando com variaveis");
+//JS é case sensitive
+
+const idade = 30;
+const nome = "Willians";
+let ano = 2020;
+
+console.log(idade, nome, ano);
+
