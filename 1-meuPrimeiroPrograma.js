80300335f737e80319b90a801740e9e9f3f7c998
Willians Santos <62728045+willianspesantos@users.noreply.github.com>
2020-07-22 12:15:58 -0300

    primeiro programa JS
    Atribuindo valor as variáveis.

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,9 @@
+console.log("meu primeiro programa: Trabalhando com variaveis");
+
+const idade = 30;
+
+console.log(idade);
+console.log(idade+2);
+
+const idadeSomada = idade + 2;
+console.log(idadeSomada);
