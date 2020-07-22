75a39907aea38a2b7bfefcb979be9057b9ded3c5
Willians Santos <62728045+willianspesantos@users.noreply.github.com>
2020-07-22 13:29:14 -0300

    Converção de Variáveis
    Tipos de converção

---
+++ 4-convertendoVariaveis.js
@@ -0,0 +1,14 @@
+console.log("Convertendo variaveis");
+
+console.log("2" + "2"); // Aqui ha uma concatenação, pois entre "" é considerado texto.
+
+console.log("ano" + "2020");
+
+console.log("10" / "2"); // o Js automaticamente reconheceu como uma operação matemática.
+
+console.log("willians" / "2"); // NaN -> não é um número.
+
+console.log(parseInt("2") + parseInt("2"));
+
+console.log(5.5);
+console.log(5,5); // números com casa decimal tem que ser colados com "." ponto e não a "," virgula.
