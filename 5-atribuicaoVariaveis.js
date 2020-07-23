2a643340a31bc5ab72635d2d4ab1777b9835f992
Willians Santos <62728045+willianspesantos@users.noreply.github.com>
2020-07-23 14:07:27 -0300

    Trabalhando com variáveis
    Diferença de declaração de variáveis: Let e const.

---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,15 @@
+console.log("meu primeiro programa: Trabalhando com variaveis");
+
+const primeiroNome = "Willians";
+const sobrenome = "Santos";
+let cidade = "Niterói";
+
+console.log(primeiroNome + sobrenome);
+console.log(primeiroNome + " " + sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+// primeiroNome = primeiroNome + sobrenome;
+//console.log(primeiroNome); não conseguirar atribuir novoo valor a variavel porque definimos ela como constante, para isso temos que usar o (let).
+
+cidade = cidade + " RJ";
+console.log(cidade);
