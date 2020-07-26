c96302f53492297af13b274ede12e17a67fe9f74
Willians Santos <62728045+willianspesantos@users.noreply.github.com>
2020-07-26 16:47:01 -0300

    Inicio do sistema de banco
    Inicio da criação do sistema para um sistema de banco.

---
+++ Introdução a orientação a objetos/index.js
@@ -0,0 +1,16 @@
+//Entendendo o problema do Cliente
+
+const cliente1 = "Willians";
+const cliente1Cpf = 11122233389;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2 = "Ana";
+const cliente2Cpf = 88822233389;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+const cliente3 = "Ana";
+const cliente3Cpf = 77722233389;
+const cliente3Agencia = 1001;
+const cliente3Saldo = 0;
