f2de15f834c147a1f5bdba4c6821d712faf22ba9
Willians Santos <62728045+willianspesantos@users.noreply.github.com>
2020-07-26 11:42:28 -0300

    Incrementando o código
    Utizando o break.

--- 8-loops.js
+++ 8-loops.js
@@ -1,5 +1,6 @@
+//Programa que verifica se esta disponivel o destino que o cliente escolheu.
 
-console.log("OperadoresLogicosEncadeados");
+console.log("OperadoresLogicosEncadeados \n");
 
 const listaDeDestinos = new Array(`Rio de Janeiro`, `São Paulo`, `Bahia`);
 console.log(listaDeDestinos);
@@ -7,15 +8,16 @@ console.log(listaDeDestinos);
 const idadeComprador = 30;
 const estarAcompanhado = true;
 const destinoEscolhido = "São Paulo";
+let destinoExiste = false;
 let contador = 0;
 
 while (contador < 3) {
     if (destinoEscolhido == listaDeDestinos[contador]) {
-        console.log("Cidade disponivel");
-    }else{
-        console.log("Destino Indisponivel!");
+        destinoExiste = true;
+        break; // força uma saida do laço de repetição
     }
-    contador = contador + 1;
+    contador += 1;
 }
 
+console.log("\n Destino existe: ", destinoExiste);
 
